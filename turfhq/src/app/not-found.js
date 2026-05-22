f868471f9fcd61532b51e43317ba41c752d5b9c0
Saumel-Das <saumel.spr@gmail.com>
2026-05-22 22:56:34 +0600

    updated ui on error page

--- turfhq/src/app/not-found.js
+++ turfhq/src/app/not-found.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Error = () => {
@@ -6,6 +7,7 @@ const Error = () => {
             <div className='flex flex-col min-h-screen justify-center items-center'>
                 <h1 className='text-7xl text-green-500 font-bold'>404 ERROR</h1>
                 <h1 className='font-bold text-3xl'>Page Not Found</h1>
+                <Link href={"/"} className='transition-all ease-in-out scale-100 mt-5'><button className='text-white bg-green-600 px-3 py-1 rounded-full cursor-pointer font-semibold transition-all duration-300 ease-out hover:bg-green-500 hover:scale-105 hover:shadow-[0_0_20px_rgba(34,197,94,0.6)] active:scale-95'>Back to Home</button></Link>
             </div>
         </div>
     );
